b35610274aa3e6a30648d6a6f1f79b025972e23f
CarolAle24 <166537489+CarolAle24@users.noreply.github.com>
2024-04-18 14:40:10 -0400

    Tarea 1 de Java S

---
+++ script.js
@@ -0,0 +1,10 @@
+let num1 =1;
+let num2 =2;
+let num3 =3;
+if(num1>num2 && num1>num3) {
+    console.log(num1 + " es el numero mayor") 
+} else if(num2>num1 && num2>num3) {
+    console.log(num1 + " es el numero mayor")
+} else {
+    console.log(num3 + " es el numero mayor")
+}
